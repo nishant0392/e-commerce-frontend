3ef748aae49c4542fb7aad399ad108cba844bb4e
nishant0392 <nishkr0392@gmail.com>
2019-09-09 18:57:58 +0530

    first commit

---
+++ src/assets/js/signup-helper.js
@@ -0,0 +1,6 @@
+
+function autoFill() {
+    var element = document.getElementById("country-select");
+    var text = element.options[element.selectedIndex].value;
+    document.getElementById("country-code").value = text;
+}
